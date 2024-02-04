90ef3f48aebee3014fe50ad000beed67b548ef37
Hajer Helali <hajerhelali@MacBook-Pro-de-Hajer.local>
2024-02-03 22:45:59 -0500

    Bouton normalement finito et onClick finito

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,51 @@
 import './App.css';
 import ButtonDonBox from "./Components/Button/donation_boxes";
+import ButtonDonFood from "./Components/Button/food_donation";
+import ButtonTrashCan from "./Components/Button/Trashcans";
 import Header from "./Components/Header/header";
 
 const App = () => {
+  const [filterType, setFilterType] = useState(null);
+    const ButtonClickDonBox = () => {
+      setFilterType("clothesCan");
+      // Vous pouvez appeler filterLocationsByDistanceAndType ici si nécessaire
+      // Exemple: filterLocationsByDistanceAndType(userLat, userLon, locations, maxDistance, filterType);
+    };
+    const ButtonClickDonFood = () => {
+      setFilterType("foodCan");
+      // Vous pouvez appeler filterLocationsByDistanceAndType ici si nécessaire
+      // Exemple: filterLocationsByDistanceAndType(userLat, userLon, locations, maxDistance, filterType);
+    }
+    const ButtonClickTrashCan = () => {
+      setFilterType("trashCan");
+      // Vous pouvez appeler filterLocationsByDistanceAndType ici si nécessaire
+      // Exemple: filterLocationsByDistanceAndType(userLat, userLon, locations, maxDistance, filterType);
+    }
+
+
   return (
     <div className="App">
       <Header />
       
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
       <div>
         <ButtonDonBox 
         text = "Boîte de dons"
+        onClick = {ButtonClickDonBox}
         style={{ 
-          position: 'absolute', 
           top: '300px', 
           left: '300px' 
         }}/>
-        <ButtonDonBox 
+        <ButtonDonFood 
         text = "Boîte de dons"
+        onClick = {ButtonClickDonFood}
         style={{ 
-          position: 'absolute', 
           top: '300px', 
           left: '600px' 
         }}/>
-        <ButtonDonBox 
+        <ButtonTrashCan 
         text = "Boîte de dons"
+        onClick = {ButtonClickTrashCan}
         style={{ 
-          position: 'absolute', 
           top: '300px', 
           left: '900px' 
         }}/>
