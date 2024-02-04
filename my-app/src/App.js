29263ba43ee1cd4dc9acc8f5290cd563b06e2aae
aylan-t <aylan.tighilet@gmail.com>
2024-02-03 22:00:24 -0500

    modif fct slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-import Distance from "./Components/slider_control/slider_control.js";
+import React, { useState, useEffect } from 'react';
+import LocationGrid from './components/LocationGrid';
+import Distance from './components/slider_control/';
+import locationsData from './data/locations.json';
+import { filterLocationsByDistanceAndType } from './Functions/utilityFunctions';
 
-function App() {
-  return(
-      <div> <Distance/> </div> )
-      ; }
+const App = () => {
+    const [locations, setLocations] = useState([]);
+    const [selectedType, setSelectedType] = useState(null);
+    const [userLocation, setUserLocation] = useState({ latitude: 0, longitude: 0 });
+    const [maxDistance, setMaxDistance] = useState(0);
 
+    
+
+    
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                setUserLocation({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                });
+            }, (error) => {
+                console.error("Erreur lors de l'obtention de la position : ", error);
+            });
+        }
+    }, []);
+
+    useEffect(() => {
+        const { latitude, longitude } = userLocation;
+        const filteredLocations = filterLocationsByDistanceAndType(latitude, longitude, locationsData, maxDistance, selectedType);
+        setLocations(filteredLocations);
+    }, [selectedType, userLocation, maxDistance]);
+
+    const handleDistanceChange = (newDistance) => {
+        setMaxDistance(newDistance);
+    };
+
+    return (
+        <div>
+            {/* Les boutons et autres éléments d'interface utilisateur ici */}
+            <Distance onDistanceChange={handleDistanceChange} />
+            <LocationGrid locations={locations} />
+        </div>
+    );
+};
 
 export default App;
+
