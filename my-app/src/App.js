08da82ec0cbf22da3bed2d2c683a8a91e6ff435c
KimDesroches <kim.desroches@polymtl.ca>
2024-02-03 19:13:55 -0500

    map in components folder !

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import './Components/Map.css'
+import React, { useState } from "react";
+import "./App.css";
+import MapComponent from "./Components/Map";
 
 function App() {
+  const [selectedLocation] = useState({
+    lat: 45.50492914373302,
+    lng: -73.6132153085261,
+  });
   return (
-    <div className="App">
-      <h1>Test MAP</h1>
-      <div className="map">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19626.777261253017!2d-73.6227391746846!3d45.50091505723996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc919f2a9fc4d71%3A0xda267ca95684133e!2sPolytechnique%20Montr%C3%A9al!5e0!3m2!1sfr!2sca!4v1706998070080!5m2!1sfr!2sca"
-      width="600" 
-      height="450" 
-      allowfullscreen="" 
-      loading="lazy"
-      referrerpolicy="no-referrer-when-downgrade"
-      title="myMap">
-      </iframe>
-      </div>
+    <div style={{ height: "100vh", width: "100%" }}>
+      <MapComponent selectedLocation={selectedLocation} />
     </div>
   );
 }
--- my-app/src/Components/Map.js
+++ my-app/src/Components/Map.js
@@ -1,3 +1,37 @@
-/*import './Map.css'
-import React from 'react'
-*/
+import React from "react";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import "./map.css"
+
+const GOOGLE_MAPS_KEY = "AIzaSyB1kropwH-5_iC-C5omKlgfw0XTKBUvNIU";
+
+const MapComponent = ({ selectedLocation }) => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: GOOGLE_MAPS_KEY
+  });
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+  if (loadError) return "Error";
+  if (!isLoaded) return "Maps";
+
+  return (
+    <div style={{ marginTop: "50px" }}>
+      <GoogleMap
+        mapContainerStyle={{
+          height: "800px",
+        }}
+        center={selectedLocation}
+        zoom={14}
+        onLoad={onMapLoad}
+      >
+        <MarkerF
+          position={selectedLocation}
+          /*icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}*/
+        />
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default MapComponent;
